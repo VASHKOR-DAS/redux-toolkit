3e0651af9c0d80b91be7f8049092bfd3b16e2e22
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-02-03 00:22:37 +0600

    Slice created

--- src/pages/Home/Products/Products.jsx
+++ src/pages/Home/Products/Products.jsx
@@ -12,17 +12,33 @@ const Products = () => {
             const data = await res.json();
 
             console.log(data);
+
+            setProducts(data);
         }
 
         fetchProducts();
     }, []);
 
 
-    return (
-        <div>
-            Products...
-        </div>
-    );
+    return <div className='productsWrapper'>
+        {
+            products.map((product) => (
+
+                <div className='card' key={product.id}>
+                    
+                    <img src={product.image} alt="" />
+
+                    <h4>{product.title}</h4>
+
+                    <h4>{product.price}</h4>
+
+                    <button className='btn'>Add to cart</button>
+
+
+                </div>
+            ))
+        }
+    </div>
 };
 
 export default Products;
---
+++ src/store/cartSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const cartSlice = createSlice({  // createSlice amader jnno reducers toiri kore action o toiri kore
+
+    name: 'cart',
+
+    //ata parameter seta up a set ase
+    initialState,
+
+    //ata diye amader state change kora jabe
+    // reducers holo pure function, pure function holo, jeta reducer a thakbe seta chara bairer kono jinis change korbe na
+    // add function diye cart a product add korbo
+    // remove function diye cart theke ta remove korbo
+    reducers: {
+        add(state, action) {    //state bolbe initialState er array k bojhay
+            state.push(action.payload);     // initialState er array te kono action hole seta push krbo
+
+        },
+
+        remove(state, action) {
+            state.filter(item => item.id !== action.payload); // jei id te click kora hobe, seta state array theke remove hye jabe
+        },
+    },
+
+});
+
+
+export const { add, remove } = cartSlice.actions;
+export default cartSlice.reducer;
