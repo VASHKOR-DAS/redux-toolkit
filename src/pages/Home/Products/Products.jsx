af9e878e0411a5e3a4b4a7b26f9909e510389eaf
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-02-03 01:23:43 +0600

    successfully selected item send to store by handleAdd function

--- src/pages/Home/Products/Products.jsx
+++ src/pages/Home/Products/Products.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { add } from '../../../store/cartSlice';
 
 const Products = () => {
+    const dispatch = useDispatch(); // handleAdd er jonno
 
     const [products, setProducts] = useState([]);
 
@@ -20,19 +23,49 @@ const Products = () => {
     }, []);
 
 
+
+    // jei product a click korbo seta add hobe, tai product ta parameter hisebe nilam
+
+    // ai selected product ta store a joma rakhte hobe, tarpor sekhan theke amder kaj korte hobe
+
+    /**
+     jevebe data store a joma rakhte hoy,
+     amader application theke 1ta action despatch korte hoy, tar vitor data o send korte pari, 
+     r sei action amder j reducer ase take call kore
+
+
+     like, cartSlice.js a
+     export const { add, remove } = cartSlice.actions; agulo amader action
+     ai action er vitor theke add action k call korle,
+
+      add(state, action) {    
+            state.push(action.payload);
+        },
+
+     ai reducer ta call hobe.
+
+     and kono data pathale seta uporer (action.payload) er vitore giye tar state ta (state.push) change kore dibe.
+
+     */
+
+
+    const handleAdd = (product) => {
+        dispatch(add(product));  // dispatch(add(ata payload, mane click korle jeta store a pathabo));
+    }
+
     return <div className='productsWrapper'>
         {
             products.map((product) => (
 
                 <div className='card' key={product.id}>
-                    
+
                     <img src={product.image} alt="" />
 
                     <h4>{product.title}</h4>
 
                     <h4>{product.price}</h4>
 
-                    <button className='btn'>Add to cart</button>
+                    <button className='btn' onClick={() => handleAdd(product)}>Add to cart</button>
 
 
                 </div>
